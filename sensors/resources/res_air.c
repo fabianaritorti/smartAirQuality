7b5affd365f7b9770e0103f49a93daf73ca78226
fabianaritorti <fabiana.ritorti@gmail.com>
2021-06-15 11:51:21 +0200

    UPDATE

--- sensors/resources/res_air.c
+++ sensors/resources/res_air.c
@@ -32,29 +32,50 @@ static void res_event_handler(void) {
 }
 
 static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset){
-    unsigned int accept = -1;
+
+
+	if(request != NULL){
+		LOG_DBG("Observing handler number %d\n", counter); 
+	}
+
+	
+
+
+	unsigned int accept = -1;
 	coap_get_header_accept(request, &accept);
 
-	if(accept == -1)
-		accept = APPLICATION_JSON;
-
-	if(accept == APPLICATION_XML) {
-		coap_set_header_content_format(response, APPLICATION_XML);
-		snprintf((char *)buffer, COAP_MAX_CHUNK_SIZE, "<state=\"%d\"/>", lightOn);
-		coap_set_payload(response, buffer, strlen((char *)buffer));
-    
-	} else if(accept == APPLICATION_JSON) {
-		coap_set_header_content_format(response, APPLICATION_JSON);
-		snprintf((char *)buffer, COAP_MAX_CHUNK_SIZE, "{\"state\":%d}", lightOn);
-		coap_set_payload(response, buffer, strlen((char *)buffer));
-  
+	if(accept == TEXT_PLAIN) {
+		coap_set_header_content_format(response, TEXT_PLAIN);
+  		coap_set_payload(response, buffer, snprintf((char *)buffer, preferred_size, "EVENT %lu", (unsigned long) counter));
 	} else {
 		coap_set_status_code(response, NOT_ACCEPTABLE_4_06);
-		const char *msg = "Supporting content-type application/json";
+		const char *msg = "Supporting content-types text/plain";
 		coap_set_payload(response, msg, strlen(msg));
-  }
-
+	}
 }
+//     unsigned int accept = -1;
+// 	coap_get_header_accept(request, &accept);
+
+// 	if(accept == -1)
+// 		accept = APPLICATION_JSON;
+
+// 	if(accept == APPLICATION_XML) {
+// 		coap_set_header_content_format(response, APPLICATION_XML);
+// 		snprintf((char *)buffer, COAP_MAX_CHUNK_SIZE, "<state=\"%d\"/>", lightOn);
+// 		coap_set_payload(response, buffer, strlen((char *)buffer));
+    
+// 	} else if(accept == APPLICATION_JSON) {
+// 		coap_set_header_content_format(response, APPLICATION_JSON);
+// 		snprintf((char *)buffer, COAP_MAX_CHUNK_SIZE, "{\"state\":%d}", lightOn);
+// 		coap_set_payload(response, buffer, strlen((char *)buffer));
+  
+// 	} else {
+// 		coap_set_status_code(response, NOT_ACCEPTABLE_4_06);
+// 		const char *msg = "Supporting content-type application/json";
+// 		coap_set_payload(response, msg, strlen(msg));
+//   }
+
+
 
 static void res_post_put_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
