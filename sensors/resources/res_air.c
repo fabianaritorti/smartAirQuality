ffcbc5d015b42dd9fa519e76f395dfe350be57d3
fabianaritorti <fabiana.ritorti@gmail.com>
2021-06-15 19:23:12 +0200

    update

--- sensors/resources/res_air.c
+++ sensors/resources/res_air.c
@@ -27,10 +27,10 @@ EVENT_RESOURCE(res_air, "title=\" Air actuator: ?POST/PUT auto=AUTO|MAN&status=O
                NULL);
  //              res_event_handler);
 
-static void res_event_handler(void) {
-	LOG_DBG("sending notification");
-  	coap_notify_observers(&res_air);
-}
+// static void res_event_handler(void) {
+// 	LOG_DBG("sending notification");
+//   	coap_notify_observers(&res_air);
+// }
 
 static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset){
 
