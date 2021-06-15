e68691e36ae898a7b9f30a19b20b3a939098566b
fabianaritorti <fabiana.ritorti@gmail.com>
2021-06-15 12:30:28 +0200

    update

--- sensors/resources/res_quality.c
+++ sensors/resources/res_quality.c
@@ -38,7 +38,7 @@ static void res_event_handler(void){
 }
 static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset){
 
-	print("CIAO")
+	print("CIAO");
 
 
 	if(request != NULL){
