8afb6bdf4ee53662d404fc9909338585be5e6406
fabianaritorti <fabiana.ritorti@gmail.com>
2021-06-18 17:54:56 +0200

    update

--- coap-server/src/main/java/server/MainApplication.java
+++ coap-server/src/main/java/server/MainApplication.java
@@ -3,36 +3,26 @@ package server;
 
 public class MainApplication {
 
-	private static MainApplication sharedInstance = new MainApplication();
-
-	public static MainApplication getSharedInstance() {
-		return sharedInstance;
-	}
+	
 
 	public static void main(String[] args) {
-		MainApplication.getSharedInstance.showMenu();
+		
+		showMenu();
 		//System.out.println("WELCOME ");
 		
 
 	}
 
-	public void showMenu() {
+	public static void showMenu() {
 		System.out.println("Welcome to the Air Depuration System!");
-	// 	System.out.println("Please, insert a command");
-	// 	System.out.println("0.Show the resources and their status");
-	// 	System.out.println("1.Start depuration");
-	// 	System.out.println("2.Stop depuration");
+		System.out.println("Please, insert a command");
+		System.out.println("0.Show the resources and their status");
+		System.out.println("1.Start depuration");
+		System.out.println("2.Stop depuration");
 	// 	//eventualmente aggiungere altro
-	// 	System.out.println("3.Exit");
-		
-
+		System.out.println("3.Exit");
 	}
 
-	// public void start() {
-	// 	//showMenu();
-	// 	System.out.println("BLA BLA");
-		
-
-	// }
+	
 
 }
