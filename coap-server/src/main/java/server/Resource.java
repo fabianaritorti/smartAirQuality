f5b7e7293ec867825fc5af26b605e88c8ab67d19
fabianaritorti <fabiana.ritorti@gmail.com>
2021-06-18 18:25:31 +0200

    update

--- coap-server/src/main/java/server/Resource.java
+++ coap-server/src/main/java/server/Resource.java
@@ -8,9 +8,44 @@ public class Resource {
 	private boolean observable;
 
 
-	public Resource() {
+	public Resource(String ip, String path, String name, boolean observable) {
 		// TODO Auto-generated constructor stub
+		this.ip = ip;
+		this.path = path;
+		this.name = name;
+		this.observable = observable;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public String getIp() {
+		return ip;
 	}
 	
+	public String getName() {
+		return name;
+	}
+
+	public boolean getObservable() {
+		return observable;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+
+	}
+
+	public String getCoapURI() {
+		return "coap://[" + this.ip + "]:5683/" + this.path;
+	}
+
+	
+
 
 }
